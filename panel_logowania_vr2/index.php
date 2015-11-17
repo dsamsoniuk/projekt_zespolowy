20a81e16c4f0cfbf7628cd349d87836a73a8faa0
DamianS <dsamsoniuk8@gmail.com>
2015-11-17 19:03:30 +0100

    Update index.php

--- panel_logowania_vr2/index.php
+++ panel_logowania_vr2/index.php
@@ -4,17 +4,11 @@ session_start();
 ini_set('session.save_path', 'panel_logowania/sesja');     // Nowa sciezka dla sesji
 require('config.php');
 require('main.class.php');
-/**************************************************************************************************************************/
 
 /* Panel logowania */
 require('panel_logowania/logowanie.class.php');      // Sprawdza czy istnieje sesja i tworzy nowa jezeli nie istnieje 
 require('panel_logowania/panel_konta.php');          // Wyglad i uklad panelu konta 
 
-/* Panel rejestracyjna */
-require('rejestracja/formularz_rejestracyjny.php');  // Wyglad formularza rejestracyjnego
-require('rejestracja/rejestracja_danych.php');       // Ogolny zarys warunkow odwolujacych sie do lasy rejestracja.class.php
-require('rejestracja/rejestracja.class.php');        // Klasa w ktorej dochodzi do sprawdzenia danych i zapisania w bazie
- 	
 /**************************************************************************************************************************/
 
 
@@ -31,21 +25,6 @@ if($msg_return == "panel_konta")
 else 
 	 panel_konta_logowania($msg_return); // zmiany wygladu panelu w pliku panel_logowania/panel_kont.php
  
-
-
-
-
-/*********************
- * REJESTRACJA 
- *********************/
-if(isset($_POST['submit_register']))panel_rejestracyjna();
-else echo '';
-formularz_rejestracyjny();
-
-
-
-
-
 
 
 
