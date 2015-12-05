45fab16cab9b48123389d595a71ef831ea594afe
DamianS <dsamsoniuk8@gmail.com>
2015-12-06 00:30:02 +0100

    Update formularz_zamowienia.php

--- zamowienia/zamowienia/formularz_zamowienia.php
+++ zamowienia/zamowienia/formularz_zamowienia.php
@@ -27,19 +27,18 @@
 	Tresc<br>
 	<textarea rows="4" cols="50" name="komentarz"></textarea><br>
 	<h2>Dostepne pakiety</h2>';
-
+	$i = 0;
 	if($pakiety != NULL){
-		for($i=0;$i < count($pakiety); $i++){
+		for($i; $i < count($pakiety); $i++){
 			echo '<h3><input type="checkbox" name="pakiet_id_'.$i.'" value="'.$pakiety[$i][0].'" />Nazwa pakietu:</h3>'.$pakiety[$i][1].', cena:'.$pakiety[$i][3].'<h3>Opis: </h3>'.$pakiety[$i][2];
-	}
+		}
 
-	echo '<br>
-	<input type="hidden" name="ilosc_pakietow" value="'.$i.'">';
 	}
 	else
 		echo 'Brak pakietow!<br>';
 
 	echo '
+	<input type="hidden" name="ilosc_pakietow" value="'.$i.'">
 	<input type="submit" name="submit" value="Wyslij zamowienie">
 	</form>';
 
